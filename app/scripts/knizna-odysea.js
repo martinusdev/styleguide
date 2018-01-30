a8fba0bef71c14314b2c99e4dfdafddfa9752b94
Thapir <klco.roman@gmail.com>
2018-01-30 12:56:45 +0100

    Asset paths

--- app/scripts/knizna-odysea.js
+++ app/scripts/knizna-odysea.js
@@ -5,19 +5,19 @@ const context = new KOContext('container');
 
 const scene = new KOScene(
   'view-1',
-  '/images/content/campaign/knizna-odysea/models/planet.json',
+  '../images/content/campaign/knizna-odysea/models/planet.json',
 );
 scene.loadScene();
 
 const scene2 = new KOScene(
   'view-2',
-  '/images/content/campaign/knizna-odysea/models/planet.json',
+  '../images/content/campaign/knizna-odysea/models/planet.json',
 );
 scene2.loadScene();
 
 const scene3 = new KOScene(
   'view-3',
-  '/images/content/campaign/knizna-odysea/models/Knihonaut-animated.json',
+  '../images/content/campaign/knizna-odysea/models/Knihonaut-animated.json',
 );
 scene3.loadModel();
 
