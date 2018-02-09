fe44a5cc31896c4b177280b73999f64c65fd9e02
Thapir <klco.roman@gmail.com>
2018-02-09 14:41:20 +0100

    Knizna odysea timeline

--- app/scripts/knizna-odysea.js
+++ app/scripts/knizna-odysea.js
@@ -1,3 +1,70 @@
-// import GGObject from './webgl/3d/_sample';
+import { nodeListToArray } from './modules/Utils';
 
-// const knihonaut = new GGObject('knihonaut');
+export default class KOTimeline {
+  constructor() {
+    this.swipers = {};
+    this.item = 0;
+    this.timelineItems = [];
+
+    window.addEventListener('DOMContentLoaded', () => {
+      window.myApp.carousels.instances.forEach(swiper => {
+        this.swipers[swiper.params.id] = swiper;
+      });
+
+      this.timelineItems = nodeListToArray(
+        document.querySelectorAll('.ko-timeline-item'),
+      );
+
+      this.swipers.planets.on('onTransitionStart', swiper =>
+        this._slideChange(swiper),
+      );
+
+      this.timelineItems.forEach(item => {
+        item.addEventListener('click', e => this._itemClick(e), false);
+      });
+    });
+
+    window.addEventListener('resize', () => this._onResize(), false);
+  }
+
+  _onResize() {
+    this.swipers.timeline._slideTo(this.item);
+  }
+
+  _itemClick(e) {
+    this.item = e.currentTarget.dataset.id;
+
+    this.swipers.planets._slideTo(this.item);
+    this._itemChange();
+  }
+
+  _slideChange(swiper) {
+    this.item = swiper.activeIndex;
+
+    this._itemChange();
+  }
+
+  _itemChange() {
+    const timelineItem = document.querySelector(`[data-id="${this.item}"]`);
+
+    this.timelineItems.forEach(item => {
+      item.classList.remove('is-active');
+    });
+    timelineItem.classList.add('is-active');
+
+    this.swipers.timeline._slideTo(this.item);
+  }
+
+  set(id) {
+    this.item = id;
+
+    this.swipers.planets._slideTo(this.item);
+    this._itemChange();
+  }
+}
+
+window.myApp.koTimeline = new KOTimeline();
+
+window.addEventListener('DOMContentLoaded', () => {
+  window.myApp.koTimeline.set(10);
+});
