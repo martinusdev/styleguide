6026a71ed2b8d1b9d51c5584b7712fba1055794a
harphield <harphield@gmail.com>
2018-03-05 16:52:14 +0100

    IE bug - tabindex musi byt prazdny, ked je modal otvoreny

--- app/scripts/modules/Modal.js
+++ app/scripts/modules/Modal.js
@@ -162,7 +162,7 @@ export default class Modal {
     this.dialog.style.zIndex =
       this.config.modalZindex + this.activeModals.length;
     this.dialog.setAttribute('aria-hidden', 'false');
-    this.dialog.setAttribute('tabindex', '0');
+    this.dialog.setAttribute('tabindex', '');
     this.dialog.addEventListener('blur', this._onBlur, true);
 
     this._handleMultipleModals();
