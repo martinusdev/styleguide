28711386de9ac09c8ec9e59c08c6b002c8eafc9d
Thapir <klco.roman@gmail.com>
2018-03-09 10:03:11 +0100

    Charts

---
+++ app/scripts/charts.js
@@ -0,0 +1,7 @@
+import Chart from 'chart.js';
+
+window.myApp = window.myApp || {};
+window.myApp.chart = Chart;
+
+window.myApp.chart.defaults.global.defaultFontFamily =
+  "'Tabac Sans', Helvetica, Arial, sans-serif";
