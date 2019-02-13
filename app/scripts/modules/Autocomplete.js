b0842c1f25c1dd1cc1a9c5be6d544ac345f99546
Dusan Tuzinsky <dusan.tuzinsky@gmail.com>
2019-02-13 14:18:40 +0100

    autocomplete i18n, fixes

--- app/scripts/modules/Autocomplete.js
+++ app/scripts/modules/Autocomplete.js
@@ -5,32 +5,48 @@
 
 import accessibleAutocomplete from 'accessible-autocomplete';
 
-const defaultConfig = {
-  autoselect: false,
-  defaultValue: undefined,
-  displayMenu: 'overlay',
-  confirmOnBlur: false,
-  minLength: 2,
-  name: 'input-autocomplete',
-  showNoOptionsFound: true,
-  placeholder: 'Začnite písať...',
-  // i18n
-  tNoResults: () => 'Nenašli sme žiadne výsledky.',
-  tStatusQueryTooShort: minQueryLength =>
-    `Minimálny počet znakov ${minQueryLength}`,
-  tStatusNoResults: () => 'Nenašli sme žiadne výsledky.',
-  tStatusSelectedOption: (selectedOption, length, index) =>
-    `Možnosť ${selectedOption} (${index + 1} z ${length}) je označená.`,
-  tStatusResults: (length, contentSelectedOption) => {
-    let message = 'výsledok zobrazený';
-    if (length > 1 && length < 5) {
-      message = 'výsledky zobrazené';
-    } else if (length >= 5) {
-      message = 'výsledkov zobrazených';
-    }
-
-    return `<span>${length} ${message}. ${contentSelectedOption}</span>`;
+const texts = {
+  sk: {
+    placeholder: 'Začnite písať...',
+    noResults: 'Nenašli sme žiadne výsledky.',
+    selectedOption: (selectedOption, length, index) =>
+      `Možnosť ${selectedOption} (${index + 1} z ${length}) je označená.`,
+    queryTooShort: minQueryLength =>
+      `Minimálny počet znakov je ${minQueryLength}`,
+    results: [
+      'výsledok zobrazený',
+      'výsledky zobrazené',
+      'výsledkov zobrazených',
+    ],
   },
+  cz: {
+    placeholder: 'Začněte psáť...',
+    noResults: 'Nenašli sme žádné výsledky.',
+    selectedOption: (selectedOption, length, index) =>
+      `Možnost ${selectedOption} (${index + 1} z ${length}) je označena.`,
+    queryTooShort: minQueryLength =>
+      `Minimální počet znaků je ${minQueryLength}`,
+    results: [
+      'výsledek zobrazen',
+      'výsledky zobrazeny',
+      'výsledků zobrazených',
+    ],
+  },
+};
+
+const defaultConfig = {
+  /** docs at https://github.com/alphagov/accessible-autocomplete */
+  autocompleteOptions: {
+    autoselect: false,
+    defaultValue: undefined,
+    displayMenu: 'overlay',
+    confirmOnBlur: false,
+    minLength: 0,
+    name: 'input-autocomplete',
+    showNoOptionsFound: true,
+  },
+  templateType: null,
+  results: [],
 };
 
 const defaultTemplates = {
@@ -38,12 +54,12 @@ const defaultTemplates = {
     inputValue: () => '', // set value of input onConfirm
     suggestion: value => `
       <div class="row">
-        <div class="col--3 col--l-5 align-self-middle pr-none">
+        <div class="col--3 align-self-middle pr-none">
           <div class="author-photo no-pad">
             <div class="portrait" style="background-image: url('${value.image}');"></div>
           </div>
         </div>
-        <div class="col--9 col--l-7 align-self-middle">
+        <div class="col--9 align-self-middle">
           <div class="h4 text-bold mb-small">${value.name}</div>
           <p class="mb-none">${value.description}</p>
         </div>
@@ -72,7 +88,7 @@ const defaultTemplates = {
     inputValue: () => '', // set value of input onConfirm
     suggestion: value => `
       <div class="row">
-        <div class="col--fill align-self-middle">
+        <div class="col--3 align-self-middle">
         <div class="product__cover collection">
           <div class="thumbnail thumbnail--book">
             ${value.images.map(image => `
@@ -95,7 +111,7 @@ const defaultTemplates = {
     suggestion: value => `
       <div class="row">
         <div class="col--12 align-self-middle">
-          <p class="mb-none">${value.description}</p>
+          <p class="mb-none">${value.name}</p>
         </div>
       </div>
     `,
@@ -106,6 +122,11 @@ export default class Autocomplete {
   constructor(element, config) {
     this.element = element;
     this.config = { ...defaultConfig, ...config };
+    this.autocompleteConfig = {
+      ...defaultConfig.autocompleteOptions,
+      ...config.autocompleteOptions,
+    };
+    this.lang = (window.myApp && window.myApp.selectLanguage) || 'sk';
 
     this.templates = defaultTemplates;
 
@@ -121,8 +142,35 @@ export default class Autocomplete {
 
     this.instance = accessibleAutocomplete({
       element: this.element,
-      templates: this.templates[type] || false,
-      ...this.config,
+      templates: this.templates[type] || undefined,
+      source: (query, populateResults) => {
+        // this can be ajax call or default values
+        const results = this.config.results;
+        const regex = new RegExp(query, 'i');
+        const filteredResults = results.filter(result => {
+          const raw = (typeof result === 'object' && result.raw) || result;
+          return raw.match(regex);
+        });
+        populateResults(filteredResults);
+      },
+      // i18n
+      placeholder: texts[this.lang].placeholder,
+      tNoResults: () => texts[this.lang].noResults,
+      tStatusQueryTooShort: texts[this.lang].queryTooShort,
+      tStatusNoResults: () => texts[this.lang].noResults,
+      tStatusSelectedOption: texts[this.lang].selectedOption,
+      tStatusResults: (length, contentSelectedOption) => {
+        const res = texts[this.lang].results;
+        let message = res[0];
+        if (length > 1 && length < 5) {
+          message = res[1];
+        } else if (length >= 5) {
+          message = res[2];
+        }
+
+        return `<span>${length} ${message}. ${contentSelectedOption}</span>`;
+      },
+      ...this.autocompleteConfig,
     });
   }
 
