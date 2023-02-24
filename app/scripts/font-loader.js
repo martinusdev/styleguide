b590890838414f26ea6fc6c3a757641f59525437
Michal Ferák <michal.ferak@martinus.sk>
2023-02-24 13:20:06 +0100

    FontFace loader

---
+++ app/scripts/font-loader.js
@@ -0,0 +1,72 @@
+if (sessionStorage.getItem('customFontLoaded')) {
+  document.documentElement.classList.add('fonts-loaded');
+} else if ('fonts' in document) {
+  const font = new FontFace(
+    'Tabac-Sans',
+    `url('../fonts/Tabac-Sans/fonts/Tabac-Sans-Regular-9531409e5f6d548c2ccc3429d9d5db69.woff2') format('woff2'),
+             url('../fonts/Tabac-Sans/fonts/Tabac-Sans-Regular-9531409e5f6d548c2ccc3429d9d5db69.woff') format('woff')`,
+    {
+      weight: 'normal',
+      style: 'normal',
+      display: 'swap',
+    }
+  );
+
+  const italicFont = new FontFace(
+    'Tabac-Sans',
+    `url('../fonts/Tabac-Sans/fonts/Tabac-Sans-Italic-939c7bc3092f331c10e75335f85f45a5.woff2') format('woff2'),
+             url('../fonts/Tabac-Sans/fonts/Tabac-Sans-Italic-939c7bc3092f331c10e75335f85f45a5.woff') format('woff')`,
+    {
+      weight: 'normal',
+      style: 'italic',
+      display: 'swap',
+    }
+  );
+
+  const semiboldFont = new FontFace(
+    'Tabac-Sans',
+    `url('../fonts/Tabac-Sans/fonts/Tabac-Sans-SemiBold-c1c5cd189c25eb46746c1d6648f58103.woff2') format('woff2'),
+             url('../fonts/Tabac-Sans/fonts/Tabac-Sans-SemiBold-c1c5cd189c25eb46746c1d6648f58103.woff') format('woff')`,
+    {
+      weight: '600',
+      style: 'normal',
+      display: 'swap',
+    }
+  );
+
+  const semiboldItalicFont = new FontFace(
+    'Tabac-Sans',
+    `url('../fonts/Tabac-Sans/fonts/Tabac-Sans-SemiBold-Italic-995d174f745ccf34d9668fc3949054e0.woff2') format('woff2'),
+             url('../fonts/Tabac-Sans/fonts/Tabac-Sans-SemiBold-Italic-995d174f745ccf34d9668fc3949054e0.woff') format('woff')`,
+    {
+      weight: '600',
+      style: 'italic',
+      display: 'swap',
+    }
+  );
+
+  const boldFont = new FontFace(
+    'Tabac-Sans',
+    `url('../fonts/Tabac-Sans/fonts/Tabac-Sans-Bold-0434c8d811e4abf9f73aab8061622b1a.woff2') format('woff2'),
+             url('../fonts/Tabac-Sans/fonts/Tabac-Sans-Bold-0434c8d811e4abf9f73aab8061622b1a.woff') format('woff')`,
+    {
+      weight: '700',
+      style: 'normal',
+      display: 'swap',
+    }
+  );
+
+  Promise.all([
+    font.load(),
+    italicFont.load(),
+    semiboldFont.load(),
+    semiboldItalicFont.load(),
+    boldFont.load()
+  ]).then((loadedFonts) => {
+    loadedFonts.forEach((loadedFont) => {
+      document.fonts.add(loadedFont);
+    });
+    document.documentElement.classList.add('fonts-loaded');
+    sessionStorage.setItem('customFontLoaded', 'true');
+  });
+}
