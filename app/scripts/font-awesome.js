235e07b44ae11cfbc555ccd4f4b778449e764ff3
Michal Ferak <michal.ferak@martinus.sk>
2024-09-10 14:42:05 +0300

    Font Awesome: Pouzivame FA kit cez package.json (#492)

--- app/scripts/font-awesome.js
+++ app/scripts/font-awesome.js
@@ -1,1 +1,5 @@
-import './plugins/font-awesome/all';
+import { library, dom } from '@fortawesome/fontawesome-svg-core'
+import { all } from '@awesome.me/kit-c68fa87f81/icons/modules'
+
+library.add(...all)
+dom.watch()
